8cd86868ff39de0423cf870ebd791eea04db5230
Patrick Carey <paddy@wackwack.co.uk>
2011-12-13 12:20:03 +0000

    Partially working now
    Each mma event directory must contain a file named 'sherdogEventID' which must contain the event's ID number as found on sherdog.com
    
    The addon is still very slow as it scans the entire library on each run, this'll be fixed in a later revision

--- default.py
+++ default.py
@@ -6,9 +6,13 @@
 
 import os
 import socket
+import sys
+import urllib
 import xbmc
 import xbmcaddon
 import xbmcgui
+import xbmcplugin
+import xbmcvfs
 
 from urllib2 import urlopen
 from BeautifulSoup import BeautifulSoup
@@ -16,6 +20,7 @@ from BeautifulSoup import BeautifulSoup
 ### get addon info
 __addon__       = xbmcaddon.Addon()
 __addonid__     = __addon__.getAddonInfo('id')
+__addonidint__  = int(sys.argv[1])
 __addonname__   = __addon__.getAddonInfo('name')
 __author__      = __addon__.getAddonInfo('author')
 __version__     = __addon__.getAddonInfo('version')
@@ -26,6 +31,9 @@ __addonpath__   = __addon__.getAddonInfo('path')
 timeout = 20
 socket.setdefaulttimeout(timeout)
 
+page = 1
+path = "/"
+
 def normalizeString( text ):
     try: text = unicodedata.normalize( 'NFKD', _unicode( text ) ).encode( 'ascii', 'ignore' )
     except: pass
@@ -35,7 +43,7 @@ def log(txt='', severity=xbmc.LOGDEBUG):
 
     """Log to txt xbmc.log at specified severity"""
     try:
-        message = ('Artwork Downloader: %s' % txt)
+        message = ('MMA Browser: %s' % txt)
         xbmc.log(msg=message, level=severity)
     except UnicodeEncodeError:
         try:
@@ -55,11 +63,20 @@ def getHtml(url):
     else:
         return data
 
-def rescanLibrary(path):
-    pathWalk = os.walk(path)
-    log( pathWalk )
+def scanLibrary(path):
+    library = []
+    idFile = 'sherdogEventID'
+    for x in os.walk(path):
+        pathIdFile = os.path.join(x[0], idFile)
+        if xbmcvfs.exists(pathIdFile):
+            event = {}
+            event['ID'] = open(pathIdFile).read()
+            event['path'] = x[0]
+            library.append(event)
+    return library
+            
 
-class getEventDetails():
+class getEventDetails:
     
     def __init__(self, sherdogEventID):
 
@@ -96,7 +113,7 @@ class getEventDetails():
                 
             rowcount = rowcount + 1
 
-class getFighterDetails():
+class getFighterDetails:
     
     def __init__(self, sherdogFighterID):
 
@@ -153,8 +170,42 @@ def getAvailableFights(sherdogFighterID, library):
                 if fighter == sherdogFighterID:
                     log('Fought on: ID: %s Name: %s' % (card.ID, card.title))
 
+def get_params():
+        param=[]
+        paramstring=sys.argv[2]
+        if len(paramstring)>=2:
+                params=sys.argv[2]
+                cleanedparams=params.replace('?','')
+                if (params[len(params)-1]=='/'):
+                        params=params[0:len(params)-2]
+                pairsofparams=cleanedparams.split('&')
+                param={}
+                for i in range(len(pairsofparams)):
+                        splitparams={}
+                        splitparams=pairsofparams[i].split('=')
+                        if (len(splitparams))==2:
+                                param[splitparams[0]]=splitparams[1]
+                                
+        return param
+
+def addLink(name,descr, url,iconimage):
+    li = xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=iconimage)
+    li.setProperty("IsPlayable", "true")
+    li.setInfo( type="Video", infoLabels={ "Title": name , "plot": descr, "plotoutline": descr} )
+    xbmcplugin.addDirectoryItem(handle=__addonidint__,url=url,listitem=li, isFolder=False)
+
+def addDir(name,path,page,iconimage):
+    u=sys.argv[0]+"?path=%s&page=%s"%(path,str(page))
+    li=xbmcgui.ListItem(name, iconImage="DefaultFolder.png",thumbnailImage=iconimage)
+    li.setInfo( type="Video", infoLabels={ "Title": name })
+    xbmcplugin.addDirectoryItem(handle=__addonidint__,url=u,listitem=li,isFolder=True)
+
+
 def doTestRun():
     # Simple test run
+    
+    library = [{ 'ID' : '9529', 'path' : '/media/raid5/mma_sorted/[2009-05-23] UFC 98: Evans vs. Machida'},{ 'ID' : '17789', 'path' : '/media/raid5/mma_sorted/[2009-05-23] UFC Live: Cruz vs. Johnson'},{ 'ID' : '16309', 'path' : '/media/raid5/mma_sorted/[2011-06-26] UFC Live: Kongo vs. Barry'},{ 'ID' : '8732', 'path' : '/media/raid5/mma_sorted/[2008-12-27] UFC 92: The Ultimate 2008'}]
+    
     card = getEventDetails('9529')
     
     log( '##### Event Details #####' )
@@ -179,6 +230,77 @@ def doTestRun():
             log( 'Country:       %s' % details.country )
             getAvailableFights(details.ID, library)
 
+def allEvents(libraryList):
+    for x in sorted(libraryList, key=lambda k: k['path']):
+        card = getEventDetails(x['ID'])
+        addDir(card.title, "/getEvent/%s" % card.ID, 1, "")
+
+def browseByOrganisation(libraryList):
+    promotionList = []
+    for x in sorted(libraryList, key=lambda k: k['path']):
+        card = getEventDetails(x['ID'])
+        if not card.promotion in promotionList:
+            promotionList.append(card.promotion)
+            addDir(card.promotion, "/browsebyorganisation/%s" % card.promotion, 1, "")
+
+def getEventsByOrganisation(libraryList, organisation):
+    eventList = []
+    for x in sorted(libraryList, key=lambda k: k['path']):
+        card = getEventDetails(x['ID'])
+        if card.promotion == organisation:
+            addDir(card.title, "/getEvent/%s" % card.ID, 1, "")
+
+def getEvent(libraryList, eventID):
+    fileList = []
+    for x in sorted(libraryList, key=lambda k: k['path']):
+        if x['ID'] == eventID:
+            thumbPath = os.path.join(x['path'], 'folder.jpg')
+            card = getEventDetails(x['ID'])
+            for root, dirs, files in os.walk(x['path']):
+                for vidFile in files:
+                    vidFileExt = os.path.splitext(vidFile)[1]
+                    vidFilePath = os.path.join(root, vidFile)
+                    if vidFileExt in ['.mkv', '.mp4', '.flv', '.avi']:
+                        addLink(vidFile, '%s: %s, %s' % (card.date, card.venue, card.city), vidFilePath, thumbPath)
+                    else:
+                        log('File ignored: %s' % vidFilePath)
+
 if (__name__ == "__main__"):
-    library = [{ 'ID' : '9529', 'path' : '/media/raid5/mma_sorted/[2009-05-23] UFC 98: Evans vs. Machida'},{ 'ID' : '17789', 'path' : '/media/raid5/mma_sorted/[2009-05-23] UFC Live: Cruz vs. Johnson'},{ 'ID' : '16309', 'path' : '/media/raid5/mma_sorted/[2011-06-26] UFC Live: Kongo vs. Barry'},{ 'ID' : '8732', 'path' : '/media/raid5/mma_sorted/[2008-12-27] UFC 92: The Ultimate 2008'}]
-    doTestRun()
+
+    params=get_params()
+
+    try:
+        page = int(urllib.unquote_plus(params["page"]))
+    except:
+        pass
+    
+    try:
+        path = urllib.unquote_plus(params["path"])
+    except:
+        pass
+    
+    if path == "/":
+        addDir("Browse by: Organisation", "/browsebyorganisation/", 1, "")
+        addDir("Browse by: Fighter", "/browsebyfighter/", 1, "")
+        addDir("All Events", "/allevents/", 1, "")
+    
+    else:
+        if path.startswith("/browsebyorganisation/"):
+            log("path:%s" % path)
+            organisation = path.replace('/browsebyorganisation/','')
+            log("organisation:%s" % organisation)
+            if organisation == '':
+                browseByOrganisation(scanLibrary('/media/raid5/mma_sorted'))
+            else:
+                getEventsByOrganisation(scanLibrary('/media/raid5/mma_sorted'), organisation)
+        elif path == "/browsebyfighter/":
+            orderby = "relevance"
+        elif path == "/allevents/":
+            allEvents(scanLibrary('/media/raid5/mma_sorted'))
+        elif path.startswith("/getEvent/"):
+            eventID = path.replace('/getEvent/','')
+            getEvent(scanLibrary('/media/raid5/mma_sorted'), eventID)
+        
+        #addDir("> Next Page", path, page+1, "")
+    
+    xbmcplugin.endOfDirectory(__addonidint__)
