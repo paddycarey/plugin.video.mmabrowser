eb0bb8434e1b4f5693158df0179c8c260cee8ae1
Patrick Carey <paddy@wackwack.co.uk>
2012-02-28 23:47:22 +0000

    Testing a workaround for dialog issue

--- default.py
+++ default.py
@@ -33,6 +33,37 @@ try:
 except:
     path = "/"
 
+# Usermenu:
+def menu():
+
+    options = ['Browse by Promotion', 'Browse by Fighter', 'All Events', 'Search']
+
+    select = xbmcgui.Dialog().select("MMA Browser", options)
+    log("Select: " + str(select))
+    if select == -1:
+        log("menu quit by user")
+        return
+    else:
+        if select == 0:
+            ## populate list of organisations
+            xbmcplugin.addSortMethod(__addonidint__, xbmcplugin.SORT_METHOD_VIDEO_TITLE)
+            xbmcplugin.addSortMethod(__addonidint__, xbmcplugin.SORT_METHOD_EPISODE)
+            browseByOrganisation()
+        elif select == 1:
+            ## populate list of fighters
+            xbmcplugin.addSortMethod(__addonidint__, xbmcplugin.SORT_METHOD_EPISODE)
+            xbmcplugin.addSortMethod(__addonidint__, xbmcplugin.SORT_METHOD_VIDEO_TITLE)
+            browseByFighter()
+        elif select == 2:
+            ## populate list of all events
+            xbmcplugin.addSortMethod(__addonidint__, xbmcplugin.SORT_METHOD_DATE)
+            xbmcplugin.addSortMethod(__addonidint__, xbmcplugin.SORT_METHOD_VIDEO_TITLE)
+            allEvents()
+        elif select == 3:
+            ## populate list of all events
+            searchAll()
+
+
 log('Script path: %s' % path)
 log('Library path: %s' % __addon__.getSetting("libraryPath"))
 
@@ -45,43 +76,24 @@ if path == "/":
     if __addon__.getSetting("checkMissingExtras") == 'true':
         library.getMissingExtras()
     library.dialog.close()
+    xbmc.sleep(1000)
     mainMenu()
 elif path.startswith("/browsebyorganisation"):
     log("path:%s" % path)
     organisation = path.replace('/browsebyorganisation','')
     log("organisation:%s" % organisation)
-    if organisation == '':
-        ## populate list of organisations
-        xbmcplugin.addSortMethod(__addonidint__, xbmcplugin.SORT_METHOD_VIDEO_TITLE)
-        xbmcplugin.addSortMethod(__addonidint__, xbmcplugin.SORT_METHOD_EPISODE)
-        browseByOrganisation()
-    else:
-        ## populate list of events for a given organisation
-        xbmcplugin.addSortMethod(__addonidint__, xbmcplugin.SORT_METHOD_DATE)
-        xbmcplugin.addSortMethod(__addonidint__, xbmcplugin.SORT_METHOD_VIDEO_TITLE)
-        getEventsByOrganisation(organisation.lstrip('/'))
+    ## populate list of events for a given organisation
+    xbmcplugin.addSortMethod(__addonidint__, xbmcplugin.SORT_METHOD_DATE)
+    xbmcplugin.addSortMethod(__addonidint__, xbmcplugin.SORT_METHOD_VIDEO_TITLE)
+    getEventsByOrganisation(organisation.lstrip('/'))
 elif path.startswith("/browsebyfighter"):
     log("path:%s" % path)
     fighterID = path.replace('/browsebyfighter','')
     log("fighterID:%s" % fighterID)
-    if fighterID == '':
-        ## populate list of fighters
-        xbmcplugin.addSortMethod(__addonidint__, xbmcplugin.SORT_METHOD_EPISODE)
-        xbmcplugin.addSortMethod(__addonidint__, xbmcplugin.SORT_METHOD_VIDEO_TITLE)
-        browseByFighter()
-    else:
-        ## populate list of all events a given fighter has fought in
-        xbmcplugin.addSortMethod(__addonidint__, xbmcplugin.SORT_METHOD_DATE)
-        xbmcplugin.addSortMethod(__addonidint__, xbmcplugin.SORT_METHOD_VIDEO_TITLE)
-        getEventsByFighter(fighterID.lstrip('/'))
-elif path == "/allevents":
-    ## populate list of all events
+    ## populate list of all events a given fighter has fought in
     xbmcplugin.addSortMethod(__addonidint__, xbmcplugin.SORT_METHOD_DATE)
     xbmcplugin.addSortMethod(__addonidint__, xbmcplugin.SORT_METHOD_VIDEO_TITLE)
-    allEvents()
-elif path == "/search":
-    ## populate list of all events
-    searchAll()
+    getEventsByFighter(fighterID.lstrip('/'))
 elif path.startswith("/getEvent/"):
     eventID = path.replace('/getEvent/','')
     ## populate list of all video files for a given event
